5a62a33934bf624c80f090c18f371a3f3c39b68b
younes.baanise <113549638+younesbaanise@users.noreply.github.com>
2024-03-06 20:36:45 +0100

    social media app

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Home, Login, Profile, Register, ResetPassword } from "./pages";
+import { useSelector } from "react-redux";
+
+
+function Layout() {
+  const { user } = useSelector(state => state.user);
+  const location = useLocation();
+  console.log(user)
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/login' state={{from: location}} replace />
+  );
+}
+
+function App() {
+  const { theme } = useSelector((state) => state.theme)
+  console.log(theme)
+
+  return (
+    <div data-theme={theme} className="w-full min-h-[100vh]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id?" element={<Profile />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import TextInput from "./TextInput";
+import Loading from "./Loading.jsx";
+import CustomButton from "./CustomButton.jsx";
+import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
+import FriendsCard from "./FriendsCard";
+import PostCard from "./PostCard";
+import EditProfile from "./EditProfile";
+
+export default{
+  Loading,
+  TextInput,
+  CustomButton,
+  TopBar,
+  ProfileCard,
+  FriendsCard,
+  PostCard,
+  EditProfile,
+};
+
+
