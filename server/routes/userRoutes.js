8c255fdf3db861a5fc4a9844f2349cea4408fe40
younes.baanise <113549638+younesbaanise@users.noreply.github.com>
2024-03-08 06:25:01 +0100

    Add files via upload

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,56 @@
+import express from "express";
+import path from "path";
+import { 
+    requestPasswordReset, 
+    resetPassword, 
+    verifyEmail, 
+    changePassword,
+    getUser,
+    updateUser,
+    friendRequest,
+    getFriendRequest,
+    acceptRequest,
+    profileViews,
+    suggestedFriends
+} from "../controllers/userController.js";
+import userAuth from "../middleware/authMiddleware.js";
+
+
+const router = express.Router();
+const __dirname = path.resolve(path.dirname(""));
+
+router.get("/verify/:userId/:token", verifyEmail);
+
+// PASSWORD RESET
+router.post("/request-passwordreset", requestPasswordReset);
+router.get("/reset-password/:userId/:token", resetPassword);
+router.post("/reset-password", changePassword);
+
+// user routes
+router.post("/get-user/:id?", userAuth, getUser);
+router.put("/update-user", userAuth, updateUser);
+
+// friedn request
+router.post("/friend-request", userAuth, friendRequest); // send
+router.post("/get-friend-request", userAuth, getFriendRequest);
+
+// accept / deny friend request
+router.post("/accept-request", userAuth, acceptRequest);
+
+// view profile
+router.post("/profile-view", userAuth, profileViews);
+
+//suggested friends
+router.post("/suggested-friends", userAuth, suggestedFriends);
+
+
+
+router.get("/verified", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+});
+
+router.get("/resetpassword", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+});
+
+export default router
