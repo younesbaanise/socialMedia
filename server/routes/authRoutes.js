dba237febeb2dd62184be947d0b92889193e8d04
younes.baanise <113549638+younesbaanise@users.noreply.github.com>
2024-03-08 06:07:29 +0100

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-export default router;
